806f4431afaa2dd89920b11e0863f2e494851d40
FeryazHacimuradov <tu201906125@code.edu.az>
2024-09-11 23:09:24 +0400

    Create docker-compose file

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -1,3 +1,6 @@
 ﻿global using Carter;
 global using MediatR;
 global using Mapster;
+global using Marten;
+global using BuildingBlocks.CQRS;
+global using Catalog.API.Models;
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//Service to the container
+
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//Conf http req pipeline
+app.MapCarter();
 
 app.Run();
