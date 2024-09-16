b3611765e7180a8c442f16ff74ee3a5e40b980f4
FeryazHacimuradov <tu201906125@code.edu.az>
2024-09-16 21:58:18 +0400

    Created Get endpoint and handler

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace Catalog.API.Products.GetProducts
+{
+    public record GetProductsQuery() : IQuery<GetProductsResult>;
+    public record GetProductsResult(IEnumerable<Product> Products);
+    public class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
+        : IQueryHandler<GetProductsQuery, GetProductsResult>
+    {
+        public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", query);
+
+            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+
+            return new GetProductsResult(products);
+        }
+    }
+}
