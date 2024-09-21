14631845d4368be364164579e2f2b1e4dfe83036
FeryazHacimuradov <tu201906125@code.edu.az>
2024-09-21 15:19:52 +0400

    Created GetProductById Handler

---
+++ src/Services/Catalog/Catalog.API/Exceptions/ProductNotFoundExaption.cs
@@ -0,0 +1,10 @@
+﻿namespace Catalog.API.Exceptions
+{
+    public class ProductNotFoundExaption : Exception
+    {
+        public ProductNotFoundExaption() : base("Product not found!")
+        {
+            
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,25 @@
+﻿
+using Catalog.API.Products.GetProducts;
+
+namespace Catalog.API.Products.GetProductById
+{
+    public record GetProductByIdQuery(Guid Id): IQuery<GetProductByIdResult>;
+    public record GetProductByIdResult(Product Product);
+    internal class GetProductByIdQueryHandler
+        (IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger)
+        : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@Query}", query);
+
+            var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+
+            if (product == null) {
+                throw new ProductNotFoundExaption();
+            }
+
+            return new GetProductByIdResult(product);
+        }
+    }
+}
