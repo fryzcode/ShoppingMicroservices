adb01afb236e8eb868950e8414df08cb8c69c98e
“FeryazHacimuradov” <tu201906125@code.edu.az>
2024-10-06 10:51:58 +0400

    Basket repository

---
+++ src/Services/Basket/Basket.API/Data/BasketRepository.cs
@@ -0,0 +1,19 @@
+namespace Basket.API.Data;
+
+public class BasketRepository : IBasketRepository
+{
+    public Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
