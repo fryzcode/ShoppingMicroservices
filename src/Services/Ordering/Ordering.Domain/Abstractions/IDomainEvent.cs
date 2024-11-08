765d6e3244465af6311a9d38f9b8cbc9f07d19e7
“FeryazHacimuradov” <tu201906125@code.edu.az>
2024-11-08 14:29:50 +0400

    create DDD abstraction

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IDomainEvent.cs
@@ -0,0 +1,10 @@
+using MediatR;
+
+namespace Ordering.Domain.Abstractions;
+
+public interface IDomainEvent : INotification
+{
+    Guid EventId => Guid.NewGuid();
+    public DateTime OccurredOn => DateTime.UtcNow;
+    public string EventType => GetType().AssemblyQualifiedName;
+}
---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IEntity.cs
@@ -0,0 +1,14 @@
+namespace Ordering.Domain.Abstractions;
+
+public interface IEntity<T> : IEntity
+{
+    public T Id { get; set; }
+}
+
+public interface IEntity
+{
+    public DateTime? CreatedDate { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime? LastModified { get; set; }
+    public string? LastModifiedBy { get; set; }
+}
