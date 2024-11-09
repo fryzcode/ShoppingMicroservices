75ac81547b65ff0866f1530c5cdcc3f57e189bcd
“FeryazHacimuradov” <tu201906125@code.edu.az>
2024-11-09 12:10:47 +0400

    implement ddd domain model for ordering microservice

---
+++ src/Services/Ordering/Ordering.Domain/GlobalUsing.cs
@@ -0,0 +1,4 @@
+global using Ordering.Domain.Abstractions;
+global using Ordering.Domain.Models;
+global using Ordering.Domain.ValueObjects;
+global using Ordering.Domain.Enums;
---
+++ src/Services/Ordering/Ordering.Domain/Models/Product.cs
@@ -0,0 +1,6 @@
+namespace Ordering.Domain.Models;
+public class Product : Entity<Guid>
+{
+    public string Name { get; private set; } = default!;
+    public decimal Price { get; private set; } = default!;
+}
