8c9a577c0a13444fd3cd5e86aecae0ba59d832dd
“FeryazHacimuradov” <tu201906125@code.edu.az>
2024-11-17 15:11:58 +0400

    created dtos

---
+++ src/Services/Ordering/Ordering.Application/Dtos/AddressDto.cs
@@ -0,0 +1,3 @@
+namespace Ordering.Application.Dtos;
+
+public record AddressDto(string FirstName, string LastName, string EmailAddress, string AddressLine, string Country, string State, string ZipCode);
