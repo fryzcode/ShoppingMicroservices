baa37268c0399ed26a72ef53ca6d9f754874b790
“FeryazHacimuradov” <tu201906125@code.edu.az>
2024-11-20 16:43:29 +0400

    develop crud operations for orders

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -1,6 +1,6 @@
-namespace Ordering.Application;
-
-public class GlobalUsing
-{
-    
-}
+global using Ordering.Domain.Models;
+global using Ordering.Domain.ValueObjects;
+global using BuildingBlocks.CQRS;
+global using Ordering.Application.Data;
+global using Ordering.Application.Dtos;
+global using Ordering.Application.Exceptions;
--- src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
@@ -1,10 +1,9 @@
-using BuildingBlocks.CQRS;
 using FluentValidation;
-using Ordering.Application.Dtos;
 
 namespace Ordering.Application.Orders.Commands.CreateOrder;
 
-public record CreateOrderCommand(OrderDto Order) : ICommand<CreateOrderResult>;
+public record CreateOrderCommand(OrderDto Order)
+    : ICommand<CreateOrderResult>;
 
 public record CreateOrderResult(Guid Id);
 
