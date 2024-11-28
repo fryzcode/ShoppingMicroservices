64ea48471c89804d2f8e986ba6bbdc10ec628d76
“FeryazHacimuradov” <tu201906125@code.edu.az>
2024-11-28 14:40:40 +0400

    create domain event handler

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using BuildingBlocks.Behaviors;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Ordering.Application;
@@ -7,9 +8,11 @@ public static class DependencyInjection
 {
      public static IServiceCollection AddApplicationServices(this IServiceCollection services)
      {
-          services.AddMediatR(cfg =>
+          services.AddMediatR(config =>
           {
-               cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+               config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+               config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+               config.AddOpenBehavior(typeof(LoggingBehavior<,>));
           });
           return services;
      }
--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -4,3 +4,6 @@ global using BuildingBlocks.CQRS;
 global using Ordering.Application.Data;
 global using Ordering.Application.Dtos;
 global using Ordering.Application.Exceptions;
+global using MediatR;
+global using Microsoft.Extensions.Logging;
+global using Ordering.Domain.Events;
