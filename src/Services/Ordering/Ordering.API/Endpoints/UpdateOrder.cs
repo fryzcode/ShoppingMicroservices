e50825e3615162c17f1987e32c3f4341b635ce8d
“FeryazHacimuradov” <tu201906125@code.edu.az>
2024-12-06 12:24:13 +0400

    Create Update Order for rest api

--- src/Services/Ordering/Ordering.API/Endpoints/UpdateOrder.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/UpdateOrder.cs
@@ -1,6 +1,29 @@
+using Ordering.Application.Orders.Commands.UpdateOrder;
+
 namespace Ordering.API.Endpoints;
 
-public class UpdateOrder
+public record UpdateOrderRequest(OrderDto Order);
+public record UpdateOrderResponse(bool IsSucess);
+
+public class UpdateOrder : ICarterModule
 {
-    
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/orders", async (UpdateOrderRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<UpdateOrderCommand>();
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<UpdateOrderResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("UpdateOrder")
+            .Produces<UpdateOrderResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Update Order")
+            .WithDescription("Update Order");
+    }
+
 }
