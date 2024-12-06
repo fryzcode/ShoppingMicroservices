2fa5ab83ccd393711eee8994fb463ee35660ad33
“FeryazHacimuradov” <tu201906125@code.edu.az>
2024-12-06 16:46:40 +0400

    Configure MassTransit

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/Events/BasketCheckoutEvent.cs
@@ -0,0 +1,21 @@
+namespace BuildingBlocks.Messaging.Events;
+public record BasketCheckoutEvent : IntegrationEvent
+{
+    public string UserName { get; set; } = default!;
+    public Guid CustomerId { get; set; } = default!;
+    public decimal TotalPrice { get; set; } = default!;
+
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+    public string EmailAddress { get; set; } = default!;
+    public string AddressLine { get; set; } = default!;
+    public string Country { get; set; } = default!;
+    public string State { get; set; } = default!;
+    public string ZipCode { get; set; } = default!;
+
+    public string CardName { get; set; } = default!;
+    public string CardNumber { get; set; } = default!;
+    public string Expiration { get; set; } = default!;
+    public string CVV { get; set; } = default!;
+    public int PaymentMethod { get; set; } = default!;
+}
