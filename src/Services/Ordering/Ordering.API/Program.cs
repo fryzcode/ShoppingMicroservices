e84841825862ae554dec6c37448663d52b03e33e
“FeryazHacimuradov” <tu201906125@code.edu.az>
2024-12-10 15:19:49 +0400

    Register MassTransit RabbitMQ into ordering.app

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -6,7 +6,7 @@ using Ordering.Infrastructure.Data.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
-    .AddApplicationServices()
+    .AddApplicationServices(builder.Configuration)
     .AddInfrastructureServices(builder.Configuration)
     .AddApiServices(builder.Configuration);
 
--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,12 +1,15 @@
 using System.Reflection;
 using BuildingBlocks.Behaviors;
+using BuildingBlocks.Messaging.MassTransit;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Ordering.Application;
 
 public static class DependencyInjection
 {
-     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+     public static IServiceCollection AddApplicationServices
+          (this IServiceCollection services, IConfiguration configuration)
      {
           services.AddMediatR(config =>
           {
@@ -14,6 +17,9 @@ public static class DependencyInjection
                config.AddOpenBehavior(typeof(ValidationBehavior<,>));
                config.AddOpenBehavior(typeof(LoggingBehavior<,>));
           });
+          
+          services.AddMessageBroker(configuration, Assembly.GetExecutingAssembly());
+               
           return services;
      }
 }
